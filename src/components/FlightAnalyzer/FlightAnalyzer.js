65175d1738ae2d431e5f38184355fa7c572aa3fc
nicomarbf <nicomarbf@v2com.mobi>
2024-12-02 01:34:49 -0300

    Progress commit

--- src/components/FlightAnalyzer/FlightAnalyzer.js
+++ src/components/FlightAnalyzer/FlightAnalyzer.js
@@ -13,15 +13,12 @@ const FlightAnalyzer = () => {
   const [result, setResult] = useState(null);
 
   const [aerodromos, setAerodromos] = useState([]);
-  const [optionsOrigem, setOptionsOrigem] = useState([]);
-  const [optionsDestino, setOptionsDestino] = useState([]);
-  const [value, setValue] = useState('');
-  const [selectedAerodromo, setSelectedAerodromo] = useState(null); // Para armazenar o valor real (SIGLA ICAO)
+  const [optionsOrigin, setOptionsOrigin] = useState([]);
+  const [optionsDestination, setOptionsDestination] = useState([]);
 
 
   useEffect(() => {
-    // Carregar dados do backend
-    axios.get('http://localhost:3001/aerodromos')
+    axios.get('http://localhost:3001/api/airports')
       .then((response) => {
         setAerodromos(response.data);
       })
@@ -35,56 +32,42 @@ const FlightAnalyzer = () => {
       .filter((aero) => aero['NOME AERÓDROMO'].toLowerCase().includes(value.toLowerCase()) ||
         aero['SIGLA ICAO AERÓDROMO'].toLowerCase().includes(value.toLowerCase()))
       .map((aero) => {
-        console.log("aero", aero)
         return ({
-          value: `${aero['NOME AERÓDROMO']} (${aero['SIGLA ICAO AERÓDROMO']})`,
+          value: `${aero['SIGLA ICAO AERÓDROMO']}`,
           label: `${aero['NOME AERÓDROMO']} (${aero['SIGLA ICAO AERÓDROMO']})`
         })
       }
-    
     );
-
     setOptions(filteredOptions);
   };
 
-  const handleSelect = (value) => {
-    // const selected = aerodromos.find(aero => aero['SIGLA ICAO AERÓDROMO'] === value);
-    // if (selected) {
-    //   setValue(`${selected['NOME AERÓDROMO']} (${selected['SIGLA ICAO AERÓDROMO']})`);
-    //   setSelectedAerodromo(selected['SIGLA ICAO AERÓDROMO']);
-    // }
-
-    console.log(value)
+  const disableDates = (current) => {
+    const today = moment().startOf('day');
+    const maxDate = moment().add(5, 'days').endOf('day');
+    return current && (current < today || current > maxDate);
   };
-
-  // const handleChange = (newValue) => {
-  //   setValue(newValue);
-  // };
 
   const handleAnalyze = async (values) => {
     setLoading(true);
     setResult(null);
 
-    try {
-      // Preparar os dados para envio
-      const { origem, destino, datetime } = values;
-      const formattedDate = moment(datetime).format('YYYY-MM-DD HH:mm:ss');
+    console.log(values)
 
+    try {
+      const { origin, destination, datetime } = values;
+      const dateObj = new Date(datetime);
       const payload = {
-        origem,
-        destino,
-        datetime: formattedDate,
+        origin,
+        destination,
+        datetime: dateObj.toLocaleString().replace(',', '')
       };
 
-      // Chamar as APIs
-      const [companyResponse, predictionResponse] = await Promise.all([
-        axios.post('http://localhost:3001/best-company', payload),
-        axios.post('http://localhost:3001/predict', payload),
+      const [flightAnalysisResponse] = await Promise.all([
+        axios.get('http://localhost:3001/api/analysis', { params: payload }),
       ]);
 
       setResult({
-        bestCompany: companyResponse.data,
-        prediction: predictionResponse.data,
+        flightAnalysis: flightAnalysisResponse.data,
       });
 
       message.success('Análise concluída com sucesso!');
@@ -109,31 +92,25 @@ const FlightAnalyzer = () => {
           <Form form={form} onFinish={handleAnalyze} layout="vertical">
             <Form.Item
               label="Aeroporto de Origem"
-              name="origem"
+              name="origin"
               rules={[{ required: true, message: 'Por favor, insira o aeroporto de origem!' }]}
             >
               <AutoComplete
                 placeholder="Selecione o aeroporto de origem"
-                // value={value}
-                onSearch={(value) => handleSearch(value, setOptionsOrigem)}
-                onSelect={handleSelect}
-                // onChange={handleChange}
-                options={optionsOrigem}
+                onSearch={(value) => handleSearch(value, setOptionsOrigin)}
+                options={optionsOrigin}
                 style={{ width: '100%' }}
               />
-              {/* Mostrar o valor da sigla ICAO (para fins de verificação) */}
-              {selectedAerodromo && <p>Sigla ICAO Selecionada: {selectedAerodromo}</p>}
             </Form.Item>
             <Form.Item
               label="Aeroporto de Destino"
-              name="destino"
+              name="destination"
               rules={[{ required: true, message: 'Por favor, insira o aeroporto de destino!' }]}
             >
               <AutoComplete
                 placeholder="Selecione o aeroporto de destino"
-                onSearch={(value) => handleSearch(value, setOptionsDestino)}
-                // onSelect={handleSelect}
-                options={optionsDestino}
+                onSearch={(value) => handleSearch(value, setOptionsDestination)}
+                options={optionsDestination}
                 style={{ width: '100%' }}
               />
             </Form.Item>
@@ -142,7 +119,13 @@ const FlightAnalyzer = () => {
               name="datetime"
               rules={[{ required: true, message: 'Por favor, insira a data e hora do voo!' }]}
             >
-              <DatePicker showTime format="YYYY-MM-DD HH:mm:ss" style={{ width: '100%' }} />
+              <DatePicker
+                showTime
+                format="DD/MM/YYYY HH:mm:ss"
+                valueFormat="YYYY-MM-DDTHH:mm:ssZ"
+                style={{ width: '100%' }}
+                disabledDate={disableDates}
+              />
             </Form.Item>
             <Form.Item>
               <Button type="primary" htmlType="submit" loading={loading} block>
